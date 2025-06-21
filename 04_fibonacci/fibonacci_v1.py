37f68c7a61f01fd9af73e97ca1f15bd589a0420e
Thomas Collart <thomas.collart@gmail.com>
2025-06-21 13:57:06 +0200

    Update fibonacci to fit video

--- 04_fibonacci/fibonacci_v1.py
+++ 04_fibonacci/fibonacci_v1.py
@@ -1,14 +1,18 @@
-def fibonacci(n):
+nb_termes = input("Combien de termes voulez-vous ? ")
+
+if nb_termes.isdigit():
+    nb_termes = int(nb_termes)
     compteur = 0
     a = 0
     b = 1
 
-    while compteur < n:
+    while compteur < nb_termes:
         print(a)
+
         temp = a
         a = b
         b = temp + b
-        compteur = compteur + 1
 
-n = int(input("Combien de termes ? "))
-fibonacci(n)
+        compteur = compteur + 1
+else:
+    print("Veuillez entrer un nombre")
--- 04_fibonacci/fibonacci_v2.py
+++ 04_fibonacci/fibonacci_v2.py
@@ -1,9 +1,15 @@
 def fibonacci(n):
     a, b = 0, 1
-    for _ in range(n):
-        print(a)
+    for i in range(n):
+        print(a, end=("\n" if i == n - 1 else " "))
         a, b = b, a + b
 
-n = int(input("Combien de termes ? "))
-fibonacci(n)
 
+if __name__ == "__main__":
+    nb_termes = input("Combien de termes voulez-vous ? ")
+
+    if nb_termes.isdigit():
+        nb_termes = int(nb_termes)
+        fibonacci(nb_termes)
+else:
+    print("Veuillez entrer un nombre")
